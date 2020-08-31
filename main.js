548f3119ba34d693b58b778b404ac37d68be1353
Darcy Price <darcyelsing@gmail.com>
2020-08-31 14:53:23 +1000

    CSS

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 window.onload = () => {
   /* WINDOW RESIZE UTIL */
-  /*   let windowWidth = $(window).width();
+  let windowWidth = $(window).width();
 
-  const handleWindowResize = () => {
+  window.addEventListener('resize', function () {
     if ($(window).width() !== windowWidth) {
       windowWidth = $(window).width();
     }
-  };
-
-  window.addEventListener('resize', handleWindowResize); */
+  });
 
   /* MOBILE NAV */
   const closeHamburgerMenu = () => {
@@ -18,12 +16,45 @@ window.onload = () => {
 
   $('.hamburger').click(closeHamburgerMenu);
 
-  /*   window.addEventListener('resize', function () {
-    console.log('windowWidth :>> ', windowWidth);
-    if (windowWidth > 600) {
-      closeHamburgerMenu();
+  /*   let resizeTimeout;
+
+  if (windowWidth < 900) {
+    $(window).resize(function () {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(function () {
+        if (windowWidth > 900) {
+          closeHamburgerMenu();
+        }
+      }, 250);
+    });
+  } */
+
+  /* NAV */
+  function addActiveClassToActivePage() {
+    let target = 0;
+
+    switch (window.location.pathname) {
+      case '/':
+        target = 1;
+        break;
+      case '/the-bridges':
+        target = 2;
+        break;
+      case '/engineers':
+        target = 3;
+        break;
+      case '/resources':
+        target = 4;
+        break;
+      case '/contact':
+        target = 5;
+        break;
     }
-  }); */
+
+    // document.getElementByTagName('nav').getElementByTagName('li')[target].classList.add('active');
+  }
+
+  addActiveClassToActivePage();
 
   /* TIMELINE */
   timeline = new TL.Timeline('timeline-embed', 'data/sample-timeline-data.json');
