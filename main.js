2a043eb38e599a199599e769fef491340e76c8b1
Darcy Price <darcyelsing@gmail.com>
2020-09-04 13:28:35 +1000

    CSS

--- main.js
+++ main.js
@@ -26,45 +26,5 @@ window.onload = () => {
     };
 
     $('.hamburger').click(closeHamburgerMenu);
-
-    /*   let resizeTimeout;
-	
-		if (windowWidth < 900) {
-			$(window).resize(function () {
-				clearTimeout(resizeTimeout);
-				resizeTimeout = setTimeout(function () {
-					if (windowWidth > 900) {
-						closeHamburgerMenu();
-					}
-				}, 250);
-			});
-		} */
-
-    /* NAV */
-    function addActiveClassToActivePage() {
-      let target = 0;
-
-      switch (window.location.pathname) {
-        case '/':
-          target = 1;
-          break;
-        case '/the-bridges':
-          target = 2;
-          break;
-        case '/engineers':
-          target = 3;
-          break;
-        case '/resources':
-          target = 4;
-          break;
-        case '/contact':
-          target = 5;
-          break;
-      }
-
-      // document.getElementByTagName('nav').getElementByTagName('li')[target].classList.add('active');
-    }
-
-    addActiveClassToActivePage();
   }, 500);
 };
