56a234d8f6c6a0451d82dd1c3c1bb3ae08699911
Darcy Price <darcyelsing@gmail.com>
2020-09-05 17:15:51 +1000

    slider CSS

--- js/main.js
+++ js/main.js
@@ -1,44 +1,48 @@
-window.onload = () => {
-  setTimeout(() => {
-    /* WINDOW RESIZE UTIL */
-    let windowWidth = $(window).width();
+/* WINDOW RESIZE UTIL */
+let windowWidth = $(window).width();
 
-    window.addEventListener('resize', function () {
-      if ($(window).width() !== windowWidth) {
-        windowWidth = $(window).width();
-      }
-    });
+window.addEventListener('resize', function () {
+  if ($(window).width() !== windowWidth) {
+    windowWidth = $(window).width();
+  }
+});
 
-    /* MOBILE NAV */
-    const closeHamburgerMenu = () => {
-      $('.hamburger').toggleClass('is-active');
-      $('.nav-mobile').slideToggle();
-    };
+/* TIMELINE */
+if (document.body.contains(document.getElementById('timeline-embed'))) {
+  timeline = new TL.Timeline('timeline-embed', 'data/sample-timeline-data.json');
+}
 
-    $('.hamburger').click(closeHamburgerMenu);
-
-    /* SLIDER */
-    if (document.body.contains(document.getElementById('splide'))) {
-      const splide = new Splide('#splide', {
-        type: 'loop',
-        padding: {
-          right: '5rem',
-          left: '5rem',
-        },
-        heightRatio: 0.5,
-        pagination: false,
-        lazyLoad: 'nearby',
-      });
-
-      splide.mount();
-
-      splide.on('click', function (e) {
-        console.log(e);
-        // TODO zoom that pic
-      });
-    }
-
-    /* FLIP CARDS */
-    $('.flip-card').flip({ axis: 'x', reverse: true });
-  }, 500);
+/* MOBILE NAV */
+const closeHamburgerMenu = () => {
+  $('.hamburger').toggleClass('is-active');
+  $('.nav-mobile').slideToggle();
 };
+
+$('.hamburger').click(closeHamburgerMenu);
+
+/* SLIDER */
+if (document.body.contains(document.getElementById('splide'))) {
+  const splide = new Splide('#splide', {
+    type: 'loop',
+    padding: {
+      right: '5rem',
+      left: '5rem',
+    },
+    heightRatio: 0.5,
+    pagination: false,
+    lazyLoad: 'nearby',
+  });
+
+  splide.mount();
+
+  splide.on('click', function (e) {
+    console.log(e);
+    // TODO zoom that pic
+  });
+}
+
+/* FLIP CARDS */
+jQuery('.flip-card').flip({ axis: 'x', reverse: true });
+
+/* IMAGE POPUPS */
+jQuery('.magnific-popup').magnificPopup({ type: 'image' });
